0e2769970a5287282907691e9c597cfb81054b72
breezebee <60617409+breezebee@users.noreply.github.com>
2020-06-09 11:43:46 +0900

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -202,3 +202,24 @@ void searchSnackPrice(Snack *s, int count){
 		printf("검색된 데이터 없음!");
 	}
 } 
+void searchSnackStarNum(Snack *s, int count){
+	int scount = 0;
+	int min=0, max=0;
+	printf("검색할 최소, 최대 별 개수를 입력하세요\n");
+	printf("최소별점: ");
+	scanf("%d", &min);
+	printf("최대별점: ");
+	scanf("%d", &max);
+	printf("\n번호    가격  중량 표준가격 별점수  제품명");
+	printf("\n========================================\n");	for(int i =0; i<count; i++){
+		if(s[i].starNum >= min && s[i].starNum <= max){
+			printf("%2d", i+1);
+			readSnack(s[i]);
+			printf("\n");
+			scount++;
+		}
+	}
+	if(scount == 0){
+		printf("검색된 데이터 없음!");
+	}
+} 
